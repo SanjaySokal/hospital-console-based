8117096488331fbe1d6892b3c59c6c9e853f0d95
SanjaySokal <sokalsanjay@gmail.com>
2023-04-14 15:17:27 +0530

    creating first Main file

---
+++ Main.java
@@ -0,0 +1,4 @@
+// As a Human it is very common to face medical emergencies in life/ Create an application to facilitate the
+// functionality for the patient to book an appointment with a doctor and maintain details about patient
+// and hospital/ Doctor to maintain all patient information/(The agenda of application is to replicate
+// Hospital management and you can implement your own idea in your own way)
