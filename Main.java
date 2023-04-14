a911f8cc4f562779d8ff1b93c52aec3cefc2dc24
SanjaySokal <sokalsanjay@gmail.com>
2023-04-14 15:19:45 +0530

    first welcome code

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-// As a Human it is very common to face medical emergencies in life/ Create an application to facilitate the
-// functionality for the patient to book an appointment with a doctor and maintain details about patient
-// and hospital/ Doctor to maintain all patient information/(The agenda of application is to replicate
-// Hospital management and you can implement your own idea in your own way)
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my Hospital");
+    }
+}
